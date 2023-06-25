ecb090b7b16f3ee13837d930cb3415b89c43d145
Bhavyaa Trivedi <bhavyaatrivedi2002@gmail.com>
2023-06-25 19:08:16 +0530

    using cssBaseline

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
-
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import {createTheme} from '@mui/material/styles';
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="App">
-      
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+      </ThemeProvider>
     </div>
   );
 }
